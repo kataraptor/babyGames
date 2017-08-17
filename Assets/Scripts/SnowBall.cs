53c479bbdd73103dfa0f99d643b27dfc24a6537a
kataraptor <kataraptor@gmail.com>
2017-08-17 13:37:02 -0600

    initial commit

---
+++ Assets/Scripts/SnowBall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowBall : MonoBehaviour {
+
+    public float ballSpeed;
+    private Rigidbody2D theRB;
+
+    public GameObject snowBallEffect;
+
+	// Use this for initialization
+	void Start () {
+        theRB = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("tester");
+        theRB.velocity = new Vector2(ballSpeed * transform.localScale.x, 0);
+		
+	}
+
+
+    //problems here: it still exists, it's not colliding with anything.
+    void onTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("got here");
+
+        Instantiate(snowBallEffect, transform.position, transform.rotation);
+        transform.gameObject.SetActive(false);
+    }
+}
